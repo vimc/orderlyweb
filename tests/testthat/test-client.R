08c95f3298e610ca023d164aac998260dbf02318
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-08-06 07:36:06 +0100

    Test api support functions

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -16,3 +16,34 @@ test_that("Authentication logic", {
     "Authorising with server http://localhost:8888")
   expect_equal(res1, res2)
 })
+
+
+test_that("API client must use absolute paths", {
+  cl <- test_orderlyweb_api_client()
+  expect_error(cl$GET("reports")
+               "Expected an absolute path")
+})
+
+
+test_that("API client URL can use prefix, stripping slashes", {
+  expect_equal(
+    orderlyweb_api_client_url("host", 443, TRUE, "prefix", 1),
+    list(www = "https://host:443/prefix",
+         api = "https://host:443/prefix/api/v1"))
+  expect_equal(
+    orderlyweb_api_client_url("host", 443, TRUE, "/prefix/", 1),
+    list(www = "https://host:443/prefix",
+         api = "https://host:443/prefix/api/v1"))
+})
+
+
+test_that("API client URL ignores empty prefix", {
+  cmp <- list(www = "https://host:443",
+              api = "https://host:443/api/v1")
+  expect_equal(
+    orderlyweb_api_client_url("host", 443, TRUE, "", 1), cmp)
+  expect_equal(
+    orderlyweb_api_client_url("host", 443, TRUE, "/", 1), cmp)
+  expect_equal(
+    orderlyweb_api_client_url("host", 443, TRUE, NULL, 1), cmp)
+})
