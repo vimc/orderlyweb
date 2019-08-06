5d3a36354fcc57c4743a95c5d2661bdf3ca1f53f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-08-06 07:51:29 +0100

    Test utilities

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -5,3 +5,13 @@ test_that("or", {
   expect_true(NULL %||% TRUE)
   expect_false(FALSE %||% TRUE)
 })
+
+
+test_that("trim string", {
+  expect_equal(trim_string("this is a long string", 80),
+               "this is a long string")
+  expect_equal(trim_string("this is a long string", 15),
+               "this is a l ...")
+  expect_equal(trim_string("this is a long string", 15, "..."),
+               "this is a lo...")
+})
